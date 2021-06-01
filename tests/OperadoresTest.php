4a8625a81d39767516b3c91e36a62b5a17a6bc3b
dwinkler <danielbwo@gmail.com>
2021-06-01 10:33:20 +0200

    tests de operadores booleanos

--- tests/OperadoresTest.php
+++ tests/OperadoresTest.php
@@ -26,22 +26,22 @@ final class OperadoresTest extends TestCase
         assertEquals($expectedResult, $result);
     }
 
-    // public function testOperadoresBooleanos(): void
-    // {
-    //     // Operadores lógicos
-    //     assertEquals(true, 15 > 7, 'Mayor que');
-    //     assertEquals(false, 15 < 7, 'Menor que');
-    //     assertEquals(true, 15 >= 7, 'Mayor o igual que');
-    //     assertEquals(false, 15 <= 7, 'Menor o igual que');
-    //     assertEquals(true, 15 == 15, 'Igual que');
-    //     assertEquals(true, 15 == '15', 'Igual que');
-    //     assertEquals(true, 15 === 15, 'Estrictamente igual que');
-    //     assertEquals(false, 15 === '15', 'Estrictamente igual que');
+    public function testOperadoresBooleanos(): void
+    {
+        // Operadores lógicos
+        assertEquals(true, 15 > 7, 'Mayor que');
+        assertEquals(false, 15 < 7, 'Menor que');
+        assertEquals(true, 15 >= 7, 'Mayor o igual que');
+        assertEquals(false, 15 <= 7, 'Menor o igual que');
+        assertEquals(true, 15 == 15, 'Igual que');
+        assertEquals(true, 15 == '15', 'Igual que');
+        assertEquals(true, 15 === 15, 'Estrictamente igual que');
+        assertEquals(false, 15 === '15', 'Estrictamente igual que');
 
-    //     // And, Or, Not
-    //     assertEquals(false, true && false, 'And');
-    //     assertEquals(true, true || false, 'Or');
-    //     assertEquals(false, !true, 'Not');
-    // }
+        // And, Or, Not
+        assertEquals(false, true && false, 'And');
+        assertEquals(true, true || false, 'Or');
+        assertEquals(false, !true, 'Not');
+    }
 
 }
