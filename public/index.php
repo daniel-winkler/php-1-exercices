15d358c0f2af833dd00544796078f4b60e1401f2
dwinkler <danielbwo@gmail.com>
2021-06-06 22:24:07 +0200

    intentos de cambiar el idioma a español

--- public/index.php
+++ public/index.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es-ES">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="iso-8859-1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <h1>Hola Daniel</h1>
-    <p>Hoy es<strong>
-        <?php 
+    <p>Hoy es <strong>
+        <?php // TODO: SOLUCIONAR COMO CAMBIAR EL IDIOMA A ESPAÑOL.
+            // $language = $lang['lang-ger'];
+            // echo $language;
+            // $_COOKIE['language'] = 'es';
+            // setcookie('language', $_COOKIE['language']);
             $today = getdate();
             echo $today['weekday']; 
         ?>
         </strong>, ¿qué tal estás?</p>
+        
 
 </body>
 </html>
