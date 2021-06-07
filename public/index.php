173a0c6b1c2374518a7e064459f9d4c7bd6e9fda
dwinkler <danielbwo@gmail.com>
2021-06-07 12:32:38 +0200

    resolvemos ejercicios y introducimos superglobales

--- public/index.php
+++ public/index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html lang="es-ES">
+<html lang="en">
 <head>
-    <meta charset="iso-8859-1">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>Hola Daniel</h1>
+   <?php include("./navbar.php")?>
+    <h1>Hola, Daniel</h1>
     <p>Hoy es <strong>
-        <?php // TODO: SOLUCIONAR COMO CAMBIAR EL IDIOMA A ESPAÑOL.
+        <?php
             // $language = $lang['lang-ger'];
             // echo $language;
             // $_COOKIE['language'] = 'es';
@@ -18,7 +19,20 @@
             echo $today['weekday']; 
         ?>
         </strong>, ¿qué tal estás?</p>
-        
 
+    <?php // https://www.php.net/manual/es/function.date.php  '<?php echo' es lo mismo que '<?='
+        $weekDays = [
+            1 => 'lunes',
+            2 => 'martes',
+            3 => 'miércoles',
+            4 => 'jueves',
+            5 => 'viernes',
+            6 => 'sábado',
+            7 => 'domingo',
+        ];
+    ?>
+    <h1>Hola, <?= $_GET['name']; ?></h1> <!-- Imprime el nombre que se ha pasado como valor por parametros a la clave 'name' en la URL (superglobales) -->
+    <p>Hoy es <strong><?= $weekDays[date('N')]; ?></strong>. ¿Qué tal estás?</p>
+        
 </body>
 </html>
--- src/ejercicios.php
+++ src/ejercicios.php
@@ -30,3 +30,54 @@ function averageAge(array $array): float {
     return $sum / count($array);
     // return array_sum(array_column($array, 'age')) / count($array);
 }
+
+// REVERSE TEST
+
+function reverseString($input) {
+    $chars = mb_str_split($input); // array de todos los caracteres del string manteniendo los caracteres 'raros' que estan fuera de la tabla ASCII (tildes, circunflejos, eñe, etc)
+    return implode("", array_reverse($chars));
+    // return strrev($input);
+}
+
+function reverseWords($sentence) {
+    $words = explode(" ", $sentence);
+    $reverseWords = array_reverse($words);
+    $reverseString = implode(" ", $reverseWords);
+    return $reverseString;
+}
+
+function reverseCharactersInWords($input) {
+    $words = explode(" ", $input);
+    $reversedArray = [];
+    foreach($words as $word) {
+        $reversedArray[] = reverseString($word);
+    }
+    $reverseString = implode(" ", $reversedArray);
+    return $reverseString;
+}
+
+// SOLUCION ZACARIAS REVERSE TEST
+
+// function reverseString (string $string): string { // $tring = Hola qué tal
+//     $chars = mb_str_split($string);
+    
+//     return implode('', array_reverse($chars));
+// }
+
+// function reverseWords (string $string): string {
+//     $words = explode(' ', $string);
+
+//     return implode(' ', array_reverse($words));
+// }
+
+// function reverseCharactersInWords (string $string): string {
+//     $words = explode(' ', $string);
+
+//     $turnWords = [];
+
+//     foreach($words as $word) {
+//         $turnWords[] = reverseString($word);
+//     }
+
+//     return implode(' ', $turnWords);
+// }
